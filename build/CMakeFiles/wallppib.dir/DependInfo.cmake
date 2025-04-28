1d59c44b75d6b474f9c688101a275d8f2da49c39
pekaala <ibrahimgnyg@gmail.com>
2025-04-29 00:50:30 +0300

    Refactor code structure for improved readability and maintainability

---
+++ build/CMakeFiles/wallppib.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ibrahim/OpenSource/wallppib/src/main.cpp" "CMakeFiles/wallppib.dir/src/main.cpp.o" "gcc" "CMakeFiles/wallppib.dir/src/main.cpp.o.d"
+  "/home/ibrahim/OpenSource/wallppib/src/wallpapermanager.cpp" "CMakeFiles/wallppib.dir/src/wallpapermanager.cpp.o" "gcc" "CMakeFiles/wallppib.dir/src/wallpapermanager.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
